e9fa84eb08fa7468b29b638bda76336db5af9e8d
yeoul <yoohozzy@gmail.com>
2025-06-04 11:04:43 +0900

    HW01 과제

---
+++ HW01.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// 함수 선언
+int calculateSum(int arr[], int size)
+{
+    int sum = 0;   //  변수 초기화
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];  // 배열의 각 요소를 합산
+    }
+    return sum;   // 합계 반환
+}
+
+double calculateAverage(int arr[], int size)
+{
+    int sum = calculateSum(arr, size);  // calculateSum 함수를 호출하여 합계 계산
+    return static_cast<double>(sum) / size;   // double로 형변환 후 평균 계산하여 반환
+}
+
+
+int main() 
+{ 
+    const int SIZE = 5;      // 배열의 크기를 5로 고정
+    int numarr[SIZE];   // 배열 선언 -> 정수 5개를 저장할 수 있는 배열
+
+    cout << "숫자 5개를 입력하세요: ";
+    for (int i = 0; i < SIZE; i++)
+    {
+        cin >> numarr[i];  // 사용자로부터 숫자 입력 받기
+    }
+
+    // 합계, 평균 계산하기
+    int sum = calculateSum(numarr, SIZE);
+    double average = calculateAverage(numarr, SIZE);
+
+    // 결과 출력하기
+    cout << "합계: " << sum << endl;
+    cout << "평균: " << average << endl;
+
+    return 0;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HW01
+
+## 요구사항
+1. 사용자로부터 숫자 5개 입력 받기  -> const int size = 5
+2. 배열에 저장  -> int numbers[size]
+3. 합계 함수 구현  -> sum
+4. 평균 함수 구현  -> average
+5. 두 함수 호출해서 결과 출력  -> cout / cin -> using namespace std;
